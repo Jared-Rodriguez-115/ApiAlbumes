6d9630303ea742ef36bd788a4d2ad1576247ec70
Sergio Jared Rodriguez Calvo <jaredeme@yahoo.com>
2022-09-16 11:31:09 -0500

    Modificando Web Api

---
+++ ApiAlbumes/Controllers/AlbumesController.cs
@@ -0,0 +1,20 @@
+﻿using ApiAlbumes.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAlbumes.Controllers
+{
+    [ApiController]
+    [Route("albumes")]
+    public class AlbumesController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Album>> Get()
+        {
+            return new List<Album>()
+            {
+                new Album() {Id=1, Nombre="Pet Sounds", Artista="The Beach Boys", Año=1966, Duracion="35:57 minutos"},
+                new Album() {Id=2, Nombre="Revolver", Artista="The Beatles", Año=1966, Duracion="35:01 minutos"}
+            };
+        }
+    }
+}
---
+++ ApiAlbumes/Entidades/Album.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiAlbumes.Entidades
+{
+    public class Album
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Artista { get; set; }
+
+        public int Año { get; set; }
+
+        public string Duracion { get; set; }
+
+    }
+}
