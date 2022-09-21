a00c6b3cdbc83194d331ed918bb5428487be062b
Sergio Jared Rodriguez Calvo <jaredeme@yahoo.com>
2022-09-21 15:00:11 -0500

    Implementacion de validaciones para cada una de las entidades

--- ApiAlbumes/Entidades/Album.cs
+++ ApiAlbumes/Entidades/Album.cs
@@ -1,15 +1,26 @@
-﻿namespace ApiAlbumes.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiAlbumes.Entidades
 {
     public class Album
     {
         public int Id { get; set; }
 
+
+        [Required(ErrorMessage = "El campo {0} es requerido obligatoriamente")]
+        [StringLength(maximumLength:10, ErrorMessage = "El campo {0} solo puede tener 10 caracteres")]
         public string Nombre { get; set; }
 
+        [Required(ErrorMessage = "El campo {0} es requerido obligatoriamente")]
+        [StringLength(maximumLength: 10, ErrorMessage = "El campo {0} solo puede tener 10 caracteres")]
         public string Artista { get; set; }
 
+        [Required(ErrorMessage = "El campo {0} es requerido obligatoriamente")]
+        [Range(1950, 2022, ErrorMessage = "El campo Año no se encuentra dentro del rango")]
         public int Año { get; set; }
 
+        [Required(ErrorMessage = "El campo {0} es requerido obligatoriamente")]
+        [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} solo puede tener 5 caracteres")]
         public string Duracion { get; set; }
 
         public List<Cancion> Canciones { get; set; }
--- ApiAlbumes/Entidades/Cancion.cs
+++ ApiAlbumes/Entidades/Cancion.cs
@@ -1,16 +1,25 @@
-﻿namespace ApiAlbumes.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiAlbumes.Entidades
 {
     public class Cancion
     {
 
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "El campo {0} es requerido obligatoriamente")]
+        [StringLength(maximumLength: 10, ErrorMessage = "El campo {0} solo puede tener 10 caracteres")]
         public string Nombre { get; set; }
 
+        [Required(ErrorMessage = "El campo {0} es requerido obligatoriamente")]
+        [StringLength(maximumLength: 10, ErrorMessage = "El campo {0} solo puede tener 10 caracteres")]
         public string Compositor { get; set; }
 
+        [Required(ErrorMessage = "El campo {0} es requerido obligatoriamente")]
+        [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} solo puede tener 5 caracteres")]
         public string Duracion { get; set; }
 
+        [Required(ErrorMessage = "El campo {0} es requerido obligatoriamente")]
         public int AlbumId { get; set; }
 
         public Album Album { get; set; }
