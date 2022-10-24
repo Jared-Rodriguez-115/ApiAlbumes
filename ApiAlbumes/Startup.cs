f850682acaac6bd902d9b348d758aaca6b1347ce
Sergio Jared Rodriguez Calvo <jaredeme@yahoo.com>
2022-10-24 00:17:51 -0500

    Modificacion Startup

--- ApiAlbumes/Startup.cs
+++ ApiAlbumes/Startup.cs
@@ -1,5 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using ApiAlbumes.Controllers;
+using ApiAlbumes.Filtros;
+using ApiAlbumes.Middlewares;
+using ApiAlbumes.Services;
+
 
 namespace ApiAlbumes
 {
@@ -20,12 +28,32 @@ namespace ApiAlbumes
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+            services.AddTransient<IService, ServiceA>();
+            services.AddTransient<ServiceTransient>();
+            services.AddScoped<ServiceScoped>();
+            services.AddSingleton<ServiceSingleton>();
+            services.AddTransient<FiltroDeAccion>();
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiAlbumes", Version = "v1" });
+            });
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+
+            app.UseResponseHttpMiddleware();
+
+            app.Map("/maping", app =>
+            {
+                app.Run(async context =>
+                {
+                    await context.Response.WriteAsync("Interceptando las peticiones");
+                });
+            });
+
             // Configure the HTTP request pipeline.
             if (env.IsDevelopment())
             {
@@ -36,6 +64,8 @@ namespace ApiAlbumes
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseResponseCaching();
 
             app.UseAuthorization();
 
